25f898af94642385741dabee5be753dca177ac1c
louispawaon <lpawaon@gmail.com>
2023-10-19 18:26:41 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+export const sum = (a,b)=>{
+    return a+b
+}
+
+export const subtract = (a,b)=>{
+    return a-b
+}
+
+export const multiply = (a,b)=>{
+    return a*b
+}
+
+export const divide = (a,b)=>{
+    return a/b
+}
